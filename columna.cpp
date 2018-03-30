228a502a9d0e2f18bf64782fc74d9c0b3d7b73dc
Sebastian <sebasbac312@gmail.com>
2018-03-30 14:12:55 -0500

    Añado la clase columna, esta clase no dibuja las columnas circulares aún

--- columna.cpp
+++ columna.cpp
@@ -1,49 +1,69 @@
-#include "colummna.h"
-void fillpoints(){
+#include "columna.h"
+void Columna::fillpoints(){
     float xpos = this->length/2;
     float ypos = this->height/2;
     float zpos = this->deep/2;
-    this->points[] = {
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
-        xpos ,-ypos, -zpos,   1.0f, 0, 0,
-        xpos ,ypos, -zpos,    1.0f, 0, 0,
-        xpos ,ypos, -zpos,    1.0f, 0, 0,
-        -xpos ,ypos, -zpos,   1.0f, 0, 0,
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
+    float tempPoints[] = {
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,ypos, -zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,ypos, -zpos,    1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
 
-        -xpos ,-ypos, zpos,   1.0f, 0, 0,
-        xpos ,-ypos, zpos,    1.0f, 0, 0,
-        xpos ,ypos, zpos,     1.0f, 0, 0,
-        xpos ,ypos, zpos,     1.0f, 0, 0,
-        -xpos ,ypos, zpos,    1.0f, 0, 0,
-        -xpos ,-ypos, zpos,   1.0f, 0, 0,
+        -xpos ,-ypos, zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, zpos,    1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, zpos,   1.0f, 0, 0, 1.0f,
 
-        -xpos ,ypos, zpos,    1.0f, 0, 0,
-        -xpos ,ypos, -zpos,   1.0f, 0, 0,
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
-        -xpos ,-ypos, zpos,   1.0f, 0, 0,
-        -xpos ,ypos, zpos,    1.0f, 0, 0,
+        -xpos ,ypos, zpos,    1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, zpos,   1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, zpos,    1.0f, 0, 0, 1.0f,
 
-        xpos ,ypos, zpos,     1.0f, 0, 0,
-        xpos ,ypos, -zpos,    1.0f, 0, 0,
-        xpos ,-ypos, -zpos,   1.0f, 0, 0,
-        xpos ,-ypos, -zpos,   1.0f, 0, 0,
-        xpos ,-ypos, zpos,    1.0f, 0, 0,
-        xpos ,ypos, zpos,     1.0f, 0, 0,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
+        xpos ,ypos, -zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
 
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
-        xpos ,-ypos, -zpos,   1.0f, 0, 0,
-        xpos ,-ypos, zpos,    1.0f, 0, 0,
-        xpos ,-ypos, zpos,    1.0f, 0, 0,
-        -xpos ,-ypos, zpos,   1.0f, 0, 0,
-        -xpos ,-ypos, -zpos,  1.0f, 0, 0,
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,-ypos, zpos,    1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, zpos,   1.0f, 0, 0, 1.0f,
+        -xpos ,-ypos, -zpos,  1.0f, 0, 0, 1.0f,
 
-        -xpos ,ypos, -zpos,   1.0f, 0, 0,
-        xpos ,ypos, -zpos,    1.0f, 0, 0,
-        xpos ,ypos, zpos,     1.0f, 0, 0,
-        xpos ,ypos, zpos,     1.0f, 0, 0,
-        -xpos ,ypos, zpos,    1.0f, 0, 0,
-        -xpos ,ypos, -zpos,   1.0f, 0, 0,
+        -xpos ,ypos, -zpos,   1.0f, 0, 0, 1.0f,
+        xpos ,ypos, -zpos,    1.0f, 0, 0, 1.0f,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
+        xpos ,ypos, zpos,     1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, zpos,    1.0f, 0, 0, 1.0f,
+        -xpos ,ypos, -zpos,   1.0f, 0, 0, 1.0f
+    };
+    for(int i = 0; i < 252; i++){
+        this->points[i]=tempPoints[i];
+    }
+}
+void Columna::draw(glm::mat4 view, glm::mat4 projection, Shader ourShader,int viewLocation, int projLocation,int modelLocation){
+    ourShader.use();
+
+    glUniformMatrix4fv(modelLocation,1,GL_FALSE,glm::value_ptr(this->model));
+    glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projLocation,1,GL_FALSE,glm::value_ptr(projection));
+    
+    glBindVertexArray(this->VAOCOLUMN); 
+        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES,0,36);
+    glBindVertexArray(0);
+}
+void Columna::verPuntos(){
+    for(int i = 0; i<216; i++){
+        cout << this->points[i] << " ";
     }
 }
--- columna.h
+++ columna.h
@@ -11,26 +11,43 @@
 #include <math.h>
 #import "shaderClass.h"
 
+using namespace std;
 class Columna{
     private:
         float width;
         float length;
         float height;
         float deep;
-        float initialPosition[3];
-        float points[216];
+        glm::vec3 initialPosition;
+        float points[252];
         //opengl elements
-        int VBO,VAO;
+        unsigned int VBOCOLUMN,VAOCOLUMN;
         glm::mat4 model;
     public:
-        Columna (float width, float length, float height, float deep){
+        Columna (glm::vec3 origin,float width, float length, float height, float deep){
             this->width = width;
             this->length = length;
             this->height = height;
             this->deep = deep;
+            this -> initialPosition = glm::vec3(origin);
+            this->fillpoints();
             this->model = glm::rotate(model, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            this->model = glm::translate(this->model, this->initialPosition);
+            glGenVertexArrays(1, &this->VAOCOLUMN);
+            glGenBuffers(1, &this->VBOCOLUMN);
+            glBindVertexArray(VAOCOLUMN);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBOCOLUMN);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(this->points), this->points, GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,7 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,7 * sizeof(float), (void*)(3* sizeof(float)));
+            glEnableVertexAttribArray(1);
         }
         void fillpoints();
-        void draw();
+        void draw(glm::mat4, glm::mat4, Shader, int, int, int);
+        void verPuntos();
 
 };
