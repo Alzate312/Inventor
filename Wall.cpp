e7a9ae90d3434a979839b1d9e4ff85e6bd339c0a
Sebastian <sebasbac312@gmail.com>
2018-04-07 15:09:37 -0500

    cambie de lado el constructor e hice pequeños cambios

--- Wall.cpp
+++ Wall.cpp
@@ -1,6 +1,27 @@
 #include "Wall.h"
+Wall::Wall(glm::vec3 origin,float width, float height, float deep){
+    this->width = width;
+    this->height = height;
+    this->deep = deep;
+    this -> initialPosition = glm::vec3(origin.x-1.0f,origin.y-1.0f,origin.z-1.0f);
+    this->fillpoints();
+    this->model = glm::rotate(model, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    this->model = glm::translate(this->model, this->initialPosition);
+    glGenVertexArrays(1, &this->VAOCOLUMN);
+    glGenBuffers(1, &this->VBOCOLUMN);
+    glBindVertexArray(VAOCOLUMN);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOCOLUMN);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(this->points), this->points, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,7 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,7 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+}
 void Wall::fillpoints(){
-    float xpos = this->length/2;
+    float xpos = this->width/2;
     float ypos = this->height/2;
     float zpos = this->deep/2;
     float tempPoints[] = {
--- Wall.h
+++ Wall.h
@@ -8,14 +8,13 @@
 //c++ libraries and the shadder class for opengl
 #include <cstdio>
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #import "shaderClass.h"
 
 using namespace std;
 class Wall{
     private:
         float width;
-        float length;
         float height;
         float deep;
         glm::vec3 initialPosition;
@@ -24,28 +23,7 @@ class Wall{
         unsigned int VBOCOLUMN,VAOCOLUMN;
         glm::mat4 model;
     public:
-        Wall (glm::vec3 origin,float width, float length, float height, float deep){
-            this->width = width;
-            this->length = length;
-            this->height = height;
-            this->deep = deep;
-            this -> initialPosition = glm::vec3(origin);
-            this->fillpoints();
-            this->model = glm::rotate(model, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-            this->model = glm::translate(this->model, this->initialPosition);
-            glGenVertexArrays(1, &this->VAOCOLUMN);
-            glGenBuffers(1, &this->VBOCOLUMN);
-            glBindVertexArray(VAOCOLUMN);
-
-            glBindBuffer(GL_ARRAY_BUFFER, VBOCOLUMN);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(this->points), this->points, GL_STATIC_DRAW);
-
-            glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,7 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
-
-            glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,7 * sizeof(float), (void*)(3* sizeof(float)));
-            glEnableVertexAttribArray(1);
-        }
+        Wall (glm::vec3,float, float, float);
         void fillpoints();
         void draw(glm::mat4, glm::mat4, Shader, int, int, int);
         void verPuntos();
